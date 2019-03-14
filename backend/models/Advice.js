765e267e19f13503d0707b54fc94f8423d20d189
MARCIANO NATHAN <marcianonathan1@gmail.com>
2019-03-14 15:06:21 +0100

    add some back

--- backend/models/Advice.js
+++ backend/models/Advice.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema; 
 
 const AdviceSchema = new Schema({
-    user_id: String,
+    user: {
+        lastname: String,
+        firstname: String
+    },
     message: String,
     rating: Number,
     date: Date
--- backend/routes/advice.js
+++ backend/routes/advice.js
@@ -3,15 +3,19 @@ const router = express.Router();
 const passport = require('passport');
 
 const Advice = require('../models/Advice');
+const User = require('../models/User');
 
-router.post('/advice', passport.authenticate('jwt', { session: false }), async (req, res) => { 
+router.post('/advice', passport.authenticate('jwt', { session: false }), async (req, res) => {
     const newAdvice = new Advice({
-        user_id: req.user._id,
         message: req.body.message,
         rating: req.body.rating,
         date: Date.now()
     });
-    const createdAdvice = await newAdvice.save();
+    const createdAdvice = await newAdvice.save()
+    await Advice.findOneAndUpdate(
+        { _id: createdAdvice._id },
+        { $set: { user: { lastname: req.user.lastname, firstname: req.user.firstname } } }
+    )
     return res.json(createdAdvice);
 })
 .get('/advice/:id', async (req, res) => {
@@ -21,7 +25,18 @@ router.post('/advice', passport.authenticate('jwt', { session: false }), async (
     return res.json(advice);
 })
 .get('/lastadvice', async (req, res) => {
-    const lastAdvices = await Advice.sort('-date').limit(3)
+    const lastAdvices = await Advice.aggregate([
+        {
+            $project:
+            {
+                "user.lastname": 1,
+                "user.firstname": 1,
+                "message": 1,
+                "rating": 1,
+                "date": 1,
+                "_id": 0
+            }
+        }]).sort('-date').limit(3)
     return res.json(lastAdvices)
 })
 .patch('/advice/:id', async (req, res) => {
